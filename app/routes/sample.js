1f753d7e2559cf235048ccd8dd4865e469e2b843
carlogilmar <carlogilmar12@gmail.com>
2018-03-07 17:18:32 -0600

    Modify mi general fucking chart 🤓

--- app/routes/sample.js
+++ app/routes/sample.js
@@ -4,6 +4,9 @@ import RSVP from 'rsvp';
 export default Route.extend({
 
 	model(){
+
+		let levels = { 1:"Nivel Latente", 2:"Nivel Impreciso", 3:"Nivel Gestándose", 4:"Nivel Produciendo" };
+
 		let chartOptions = {
 			chart: {
 				type: 'heatmap',
@@ -11,19 +14,19 @@ export default Route.extend({
 				marginBottom: 40
 			},
 			title: {
-				text: 'Sales per employee per weekday'
+				text: 'Evaluación 2017'
 			},
 			xAxis: {
-				categories: ['Alexander', 'Marie', 'Maximilian', 'Sophia', 'Lukas', 'Maria', 'Leon', 'Anna', 'Tim', 'Laura']
+				categories: ['Cultura EBC', 'Comunicación Interpersonal', 'Comunicación Intrapersonal', 'Gestión de Clase', 'Gestión Tecnológica', 'Liderazgo', 'Orientación al alumno', 'Vinculación laboral']
 			},
 			yAxis: {
-				categories: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
-				title: null
+				categories: ['Campus CMX', 'Campus LEO', 'Campus GDL', 'Campus SNL', 'Campus EMP', 'Campus VIR'],
+				title: 'Campus'
 			},
 			colorAxis: {
 				min: 0,
 				minColor: '#FFFFFF',
-				maxColor: Highcharts.getOptions().colors[0]
+				maxColor: '#000099'
 			},
 			legend: {
 				align: 'right',
@@ -35,19 +38,28 @@ export default Route.extend({
 			},
 			tooltip: {
 				formatter: function () {
-					return '<b>' + this.series.xAxis.categories[this.point.x] + '</b> sold <br><b>' +
-						this.point.value + '</b> items on <br><b>' + this.series.yAxis.categories[this.point.y] + '</b>';
+					return '<b>' + this.series.xAxis.categories[this.point.x] + '</b> en <b>' + this.series.yAxis.categories[this.point.y] + '</b> con <b>'+
+						levels[this.point.value];
 				}
 			},
 		};
 
 		let chartData = [{
-			name: 'Sales per employee',
+			name: 'Evaluaciones Generales',
 			borderWidth: 1,
-			data: [[0,0,10],[0,1,19],[0,2,8],[0,3,24],[0,4,67],[1,0,92],[1,1,58],[1,2,78],[1,3,117],[1,4,48],[2,0,35],[2,1,15],[2,2,123],[2,3,64],[2,4,52],[3,0,72],[3,1,132],[3,2,114],[3,3,19],[3,4,16],[4,0,38],[4,1,5],[4,2,8],[4,3,117],[4,4,115],[5,0,88],[5,1,32],[5,2,12],[5,3,6],[5,4,120],[6,0,13],[6,1,44],[6,2,88],[6,3,98],[6,4,96],[7,0,31],[7,1,1],[7,2,82],[7,3,32],[7,4,30],[8,0,85],[8,1,97],[8,2,123],[8,3,64],[8,4,84],[9,0,47],[9,1,114],[9,2,31],[9,3,48],[9,4,91]],
+			data: [
+							[0,0,1],[0,1,1],[0,2,4],[0,3,4],[0,4,4],
+							[1,0,2],[1,1,4],[1,2,1],[1,3,1],[1,4,3],
+							[2,0,3],[2,1,1],[2,2,2],[2,3,4],[2,4,2],
+							[3,0,4],[3,1,3],[3,2,4],[3,3,1],[3,4,1],
+							[4,0,3],[4,1,4],[4,2,3],[4,3,1],[4,4,1],
+							[5,0,4],[5,1,2],[5,2,2],[5,3,4],[5,4,2],
+							[6,0,3],[6,1,4],[6,2,4],[6,3,3],[6,4,3],
+							[7,0,1],[7,1,1],[7,2,1],[7,3,2],[7,4,3]
+						],
 			dataLabels: {
 				enabled: true,
-				color: 'black',
+				color: 'white',
 				style: {
 					textShadow: 'none'
 				}
