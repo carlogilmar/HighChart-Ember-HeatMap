174eae0a9db9f24ac1e2bd8e21f5a97c11b1aaf3
LuisSas <luis@makingdevs.com>
2018-04-06 10:47:34 -0500

    -r

--- app/routes/comparative-historical-chart.js
+++ app/routes/comparative-historical-chart.js
@@ -5,7 +5,6 @@ export default Route.extend({
 
 	model(){
 		let chartOptions = {
-			colors: ['rgba(55, 163, 235, 0.68)','rgba(74, 191, 191, 0.7)','rgba(255, 207, 87, 0.94)','rgba(255, 97, 131, 0.77)'],
 			chart: {
 				type: 'line'
 			},
@@ -13,7 +12,7 @@ export default Route.extend({
 				text: 'Grafica General de Historicos'
 			},
 			subtitle: {
-				text: 'Campus CMX'
+				text: 'Evaluaciones'
 			},
 			xAxis: {
 				categories: ['Cultura EBC', 'Comunicacion Interpersonal', 'Comunicacion Intrapersonal', 'Gestion de Clase', 'Gestion Tecnologica', 'Liderazgo', 'Orientacion al alumno', 'Vinculacion laboral']
@@ -32,20 +31,44 @@ export default Route.extend({
 				}
 			},
 			series: [{
-				name: 'Evaluacion 1',
+				name: 'LEO',
 				data: [1,2,3,4,4,3,2,1]
 			}, {
-				name: 'Evaluacion 2',
+				name: 'CHI',
 				data: [2,1,3,2,1,4,4,3]
-			},{
-				name: 'Evaluacion 3',
-				data: [2,2,2,2,2,2,2,2]
-			},{
-				name: 'Evaluacion 4',
-				data: [1,1,2,4,4,4,3,1]
-			},{
-				name: 'Evaluacion 5',
-				data: [1,1,1,1,1,1,4,3]
+			}, {
+				name: 'EMP',
+				data: [1,2,2,3,1,2,3,4]
+			}, {
+				name: 'QRO',
+				data: [1,2,3,4,4,2,1,3]
+			}, {
+				name: 'TLA',
+				data: [3,2,4,3,4,1,2,3]
+			}, {
+				name: 'TOL',
+				data: [2,3,1,4,3,2,1,4]
+			}, {
+				name: 'VIR',
+				data: [1,2,2,4,4,2,1,3]
+			}, {
+				name: 'SLP',
+				data: [1,2,2,2,4,3,2,1]
+			}, {
+				name: 'AGS',
+				data: [1,2,4,3,1,2,2,4]
+			}, {
+				name: 'PCH',
+				data: [3,2,1,3,2,2,1,3]
+			}, {
+				name: 'MER',
+				data: [1,4,1,2,4,2,3,2]
+			}, {
+				name: 'GDL',
+				data: [4,3,2,1,2,2,4,4]
+			}, {
+				name: 'CMX',
+				data: [1,4,1,2,4,3,1,2]
 			}]
 		};
 
